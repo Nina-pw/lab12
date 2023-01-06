89b03d07c8fc2e5c41fc88168ce3ddf052bdcccd
Pimmada <nina21551@gmail.com>
2023-01-06 15:38:49 +0700

    lab12_1.cpp completed

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,15 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int x = rand()%9;
+    string a[] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    cout << "You will get " << a[x] << " in this 261102.";
+}
